6aa98ce56dae44bea44ed86c41db2e377cad2564
falconlee236 <falconlee236@cau.ac.kr>
2024-09-04 14:23:30 +0900

    Update detail_page.py : milestone 1.0

--- detail_page.py
+++ detail_page.py
@@ -1,9 +1,10 @@
-from unicodedata import category
-
 from playwright.sync_api import sync_playwright
 import json
 import re
+
+from items.Menu import Menu
 from items.Review import Review
+from items.Shop import Shop
 
 # 리스트를 사용하여 모든 응답을 저장
 responses = []
@@ -18,7 +19,7 @@ def get_review(_id_str, _page, _shop_name, _review_cnt):
     if _review_cnt > 3:
         while _page.locator("div.evaluation_review a.link_more.link_unfold").count() == 0:
             _page.click("div.evaluation_review a.link_more")
-            _page.wait_for_timeout(500)
+            _page.wait_for_timeout(300)
 
     evaluation_items = _page.locator('div.evaluation_review ul.list_evaluation li')
     review_list = []
@@ -48,7 +49,32 @@ def get_review(_id_str, _page, _shop_name, _review_cnt):
 
 
 def get_menu(_id_str, _page, _shop_name):
-    pass
+    menu_elements = _page.locator('div#mArticle div.cont_menu ul.list_menu li')
+    menu_list = []
+    menu_name_list = []
+    for menu_element in menu_elements.element_handles():
+        info_menu = menu_element.query_selector("div.info_menu")
+        _name = info_menu.query_selector("span.loss_word").inner_text()
+        _price_element = info_menu.query_selector("em.price_menu")
+        _price = "" if _price_element is None else _price_element.inner_text()[4:]
+        link_photo = menu_element.query_selector("a.link_photo")
+        _url = "" if link_photo is None else "https:" + link_photo.query_selector("span.inner_photo img.img_thumb").get_attribute("src")
+
+        menu = Menu(
+            id_str=_id_str,
+            shop_name=_shop_name,
+            name=_name,
+            price=_price,
+            url=_url
+        )
+
+        menu_list.append(menu)
+        menu_name_list.append(_name)
+        print(menu)
+        print("-----" * 10)
+
+    return menu_list, menu_name_list
+
 
 if __name__ == "__main__":
     with sync_playwright() as pw:
@@ -94,18 +120,63 @@ if __name__ == "__main__":
             detail_page.goto(detail_url)
             detail_page.wait_for_load_state("networkidle")
 
-            title = detail_page.query_selector("div#mArticle div.inner_place h2.tit_location").inner_text()
-            address = detail_page.query_selector("div#mArticle div.details_placeinfo div.placeinfo_default div.location_detail span.txt_address")
-            contact = detail_page.query_selector("div#mArticle div.details_placeinfo div.placeinfo_default.placeinfo_contact div.location_detail span.txt_contact")
-            star = detail_page.query_selector("div#mArticle div.place_details div.location_evaluation span.color_b")
-            open_time_list = detail_page.query_selector_all("div#mArticle div.details_placeinfo div.placeinfo_default div.location_detail.openhour_wrap div.location_present span.txt_operation")
-            category = detail_page.query_selector("div#mArticle div.place_details div.location_evaluation span.txt_location")
-            # price =
-            description = ""
-            source = "kakao map"
+            title_element = detail_page.query_selector("div#mArticle div.inner_place h2.tit_location")
+            title = "" if  title_element is None else title_element.inner_text()
+
+            address_element = detail_page.query_selector("div#mArticle div.details_placeinfo div.placeinfo_default div.location_detail span.txt_address")
+            address = "" if address_element is None else address_element.inner_text()
+
+            contact_element = detail_page.query_selector("div#mArticle div.details_placeinfo div.placeinfo_default.placeinfo_contact div.location_detail span.txt_contact")
+            contact = "" if contact_element is None else contact_element.inner_text()
+
+            star_element = detail_page.query_selector("div#mArticle div.place_details div.location_evaluation span.color_b")
+            star = "" if star_element is None else star_element.inner_text()
+
+            category_element = detail_page.query_selector("div#mArticle div.place_details div.location_evaluation span.txt_location")
+            category = "" if category_element is None else category_element.inner_text()[4:]
+
             review_cnt_element = detail_page.query_selector("div#mArticle strong.total_evaluation span.color_b")
+            open_time_list = detail_page.query_selector_all(
+                "div#mArticle div.details_placeinfo div.placeinfo_default div.location_detail.openhour_wrap div.location_present span.txt_operation")
+            open_time = ""
+            for open_time_element in open_time_list:
+                open_time += open_time_element.inner_text()
+
+            review_cnt = 0
+            review_list = []
             if review_cnt_element:
                 review_cnt = int(review_cnt_element.inner_text())
-                get_review(id_str, detail_page, title, review_cnt)
+                review_list = get_review(id_str, detail_page, title, review_cnt)
+
+            menu_list = []
+            menu_name_list = []
+            menu_page = detail_page.query_selector("div#mArticle div.cont_menu")
+            if menu_page:
+                menu_list, menu_name_list = get_menu(id_str, detail_page, title)
+
+            shop = Shop(
+                id_str=id_str,
+                name=title,
+                address=address,
+                contact=contact,
+                star=star,
+                review_cnt=review_cnt,
+                time=open_time,
+                category=category,
+                menu_list=menu_name_list,
+                source="kakao map",
+            )
+            print("shop----------")
+            print(shop)
+            print("-----" * 10)
+            print("menu-----------")
+            for x in menu_list:
+                print(x)
+            print("-----" * 10)
+            print("review-----------")
+            for x in review_list:
+                print(x)
+            print("-----" * 10)
+
 
             detail_page.close()
